4c665e10ded48fc6fdb4b8b32fec22bb305055f9
qtipee <quent.vallat@gmail.com>
2020-03-16 17:15:31 +0100

    Default layout tabs

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,10 +16,10 @@
                     exact
                 >
                     <v-list-item-action>
-                    <v-icon>{{ item.icon }}</v-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
-                    <v-list-item-title v-text="item.title" />
+                        <v-list-item-title v-text="item.title" />
                     </v-list-item-content>
                 </v-list-item>
             </v-list>
