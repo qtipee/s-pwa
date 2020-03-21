5799e859b70e5ef14ee472eaf64a9ab2bffcbe8d
qtipee <quent.vallat@gmail.com>
2020-03-21 19:12:05 +0100

    CRUD Table test

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
     <!--FileExplorer /-->
+    <CRUDTable />
 </template>
 
 <script>
 import FileExplorer from '~/components/FileExplorer.vue'
+import CRUDTable from '~/components/CRUDTable.vue'
 
 export default {
   components: {
       FileExplorer,
+      CRUDTable,
   },
 }
 </script>
